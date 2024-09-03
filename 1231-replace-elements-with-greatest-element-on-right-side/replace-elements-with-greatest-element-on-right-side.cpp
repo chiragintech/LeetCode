88715544499ac93cd5477f23cd5ce289791e8305
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2024-09-03 14:57:18 +0530

    Time: 2359 ms (9.33%) | Memory: 71.3 MB (60.98%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
+    bool comp(int a, int b) 
+    { 
+        return (a < b); 
+    } 
     vector<int> replaceElements(vector<int>& arr) {
         //replace every element with greatest among elements to the right, last replace as -1
         int n,x;
         n = arr.size();
-        for(int i = 0; i<n; i++) {
-            if (i != (n-1)) {
-                x = arr[i+1];
-                for (int j = i+1; j<n; j++) {
-                    if (arr[j] > x) {
-                        x = arr[j];
-                    }
-                }
-                arr[i] = x;
+        int max_ele;
+        for(int i = 0;i<n;i++) { 
+            if(i != n-1) {
+                max_ele = *max_element(arr.begin() + i + 1,arr.end());
+                arr[i] = max_ele;
             }
-            else{
+            else {
                 arr[i] = -1;
             }
+            
         }
+        // for(int i = 0; i<n; i++) {
+        //     if (i != (n-1)) {
+        //         x = arr[i+1];
+        //         for (int j = i+1; j<n; j++) {
+        //             if (arr[j] > x) {
+        //                 x = arr[j];
+        //             }
+        //         }
+        //         arr[i] = x;
+        //     }
+        //     else{
+        //         arr[i] = -1;
+        //     }
+        // }
         return arr;
     }
 };
