19fccb4208214380a483bada4df793945285e0ad
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2025-03-27 12:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (44.31%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -5,7 +5,5 @@ class Solution:
         # ans[i] = nums[i] and ans[i+n] == nums[i]
         # ans = 2 nums array concatenated
         # return ans
-        n = len(nums)
-        nums = nums + nums
-        ans = nums
-        return ans
+        nums.extend(nums)
+        return nums
