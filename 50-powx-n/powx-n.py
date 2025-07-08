ca10a3717a1504725ac2f2af81acc3a9f35b7d27
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2025-07-08 13:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (94.22%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,6 +1,6 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        ans = 1.0
+        ans = 1
         n2 = n
         if n2 < 0:
             n2 = -1 * n2
@@ -12,7 +12,7 @@ class Solution:
                 x = x * x
                 n2 = n2 // 2
         if n < 0:
-            ans = 1.0 / ans
+            ans = 1 / ans
         return ans
         
         
