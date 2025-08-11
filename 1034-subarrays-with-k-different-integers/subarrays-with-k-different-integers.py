b9fcc4eba0eaa9e5c378e5beefa5d86c657e0400
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2025-08-11 18:20:24 +0530

    Time: 122 ms (76.93%) | Memory: 19.6 MB (94.52%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        def helper(x: int) -> int:
+            l = 0
+            h = {}
+            count = 0
+            for r in range(len(nums)):
+                h[nums[r]] = h.get(nums[r], 0) + 1
+                while len(h) > x:
+                    h[nums[l]] -= 1
+                    if h[nums[l]] == 0:
+                        h.pop(nums[l])
+                    l += 1
+                count += (r - l + 1)
+            return count
+        
+        return helper(k) - helper(k - 1)
