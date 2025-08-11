f2fa06c9311407c02a0832c42f1fd2df7251a243
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2025-08-11 12:18:51 +0530

    Time: 184 ms (21.52%) | Memory: 18.2 MB (23.89%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -8,7 +8,7 @@ class Solution:
                 h[s[r]] = 1
             else:
                 h[s[r]] += 1
-            while sum(h.values()) - max(h.values()) > k:
+            while (r-l+1) - max(h.values()) > k:
                 h[s[l]] -= 1
                 l += 1
             max_len = max(max_len, r-l+1)
