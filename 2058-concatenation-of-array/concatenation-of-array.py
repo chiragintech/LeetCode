7e671e78941e06b3b15242520d8d339c9140e947
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2025-08-11 22:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (98.96%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -1,9 +1,3 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        # nums - length = n
-        # ans - length = 2n
-        # ans[i] = nums[i] and ans[i+n] == nums[i]
-        # ans = 2 nums array concatenated
-        # return ans
-        nums.extend(nums)
-        return nums
+        return nums + nums
