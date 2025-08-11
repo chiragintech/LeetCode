67501c953c316ea25b98d10fe75fe1f06149e6f8
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2025-08-11 22:41:23 +0530

    Time: 70 ms (5.15%) | Memory: 34.8 MB (19.23%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,5 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # true if atleast twice in array
-        # false if every element distinct
-        return len(set(nums)) < len(nums) #if nums is more, it is going to return true
+        h = defaultdict(int)
+        for i in range(len(nums)):
+            h[nums[i]] += 1
+        if max(h.values()) > 1:
+            return True
+        return False
