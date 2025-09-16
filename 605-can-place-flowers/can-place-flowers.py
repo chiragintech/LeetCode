712489914ce750dc9d6195f193a35d7155a46442
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2025-09-16 23:17:02 +0530

    Time: 7 ms (72.74%) | Memory: 18.4 MB (14.74%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -2,25 +2,11 @@ class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         count = 0
         def isSafe(k):
-            if flowerbed[k] == 1:
-                return False
-            if len(flowerbed) == 1:
-                return True
-            if k == 0:
-                if flowerbed[k+1] != 1:
-                    return True
-                else:
-                    return False
-            if k == len(flowerbed) - 1:
-                if flowerbed[k-1] != 1:
-                    return True
-                else:
-                    return False
-            if flowerbed[k-1] == 0 and flowerbed[k+1] == 0 and flowerbed[k-1] == 0: 
+            if flowerbed[k-1] == 0 and flowerbed[k] == 0 and flowerbed[k+1] == 0:
                 return True
             return False
-            
-        for i in range(len(flowerbed)):
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(1,len(flowerbed)-1):
             if isSafe(i):
                 flowerbed[i] = 1
                 count += 1
