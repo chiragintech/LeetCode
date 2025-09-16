6191a2fb7fd16cb9a8a0afce9545e80c78feb63e
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2025-09-17 00:12:06 +0530

    Time: 8 ms (71.57%) | Memory: 18.7 MB (28.33%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -2,7 +2,7 @@ class Solution:
     def reverseVowels(self, s: str) -> str:
         l,r = 0, len(s) - 1
         l1 = list(s)
-        vowels = ['a','e','i','o','u','A','E','I','O','U']
+        vowels = set('AEIOUaeiou')
         while l < r and l < len(s) and r < len(s):
             if l1[l] in vowels and l1[r] in vowels:
                 l1[l],l1[r] = l1[r],l1[l]
